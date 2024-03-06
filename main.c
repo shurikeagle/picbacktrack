05b5921246a3a81877b74c2384570eaf85558a2a
shurikeagle <alexander.orlov22@gmail.com>
2024-03-07 01:38:52 +0400

    wip: #2 on button pressed

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "hardware/timer.h"
+#include "hardware/sync.h"
+
 #include "gps_uart.h"
 #include "geo.h"
 #include "disp_i2c.h"
@@ -29,6 +32,24 @@
 
 #pragma endregion
 
+#pragma region controls
+
+#define BUTTON_PIN 10
+#define SAVE_POINT_BTN_DURATION_SEC 3
+
+#pragma endregion
+
+// TODO: Move into separated module
+void init_button(void);
+
+// TODO: Move into separated module =========
+// TODO: Add struct in geo and use it instead
+float saved_point_ltd;
+float saved_point_lng;
+
+void on_button_pressed(uint gpio, uint32_t events);
+// ==========================================
+
 int main() {
 
     stdio_init_all();
@@ -36,6 +57,8 @@ int main() {
     gps_uart_init(GPS_UART_ID, GPS_UART_TX_PIN, GPS_UART_RX_PIN, BE_220_BAUDRATE);
 
     disp_i2c_init(DISPLAY_I2C_ID, DISPLAY_I2C_SDA_GP, DISPLAY_I2C_SCL_GP, DISPLAY_BAUDRATE);
+
+    init_button();
 
     sleep_ms(100);
 
@@ -79,20 +102,20 @@ int main() {
                     rmc_data.longitude,
                     fake_point_lat,
                     fake_point_lng);
-                disp_saved_point_info_t disp_info;
-                disp_info.lat = fake_point_lat;
-                disp_info.lng = fake_point_lng;
+                disp_saved_point_info_t save_pt_disp_info;
+                save_pt_disp_info.lat = fake_point_lat;
+                save_pt_disp_info.lng = fake_point_lng;
                 // TODO: Bad logic! Add check with short max/min values
-                disp_info.distance_m = (unsigned short) round(distance_to_fake_point);
+                save_pt_disp_info.distance_m = (unsigned short) round(distance_to_fake_point);
                 char direction_buff[3];
                 geo_cardinal_direction(
-                    disp_info.absolute_direction, 
+                    save_pt_disp_info.absolute_direction, 
                     rmc_data.latitude,
                     rmc_data.longitude,
                     fake_point_lat,
                     fake_point_lng);
 
-                disp_i2c_show_saved_point(disp_info);
+                disp_i2c_show_saved_point(save_pt_disp_info);
             } else {
                 disp_i2c_clear_saved_point();
             }
@@ -100,4 +123,40 @@ int main() {
 
         sleep_ms(1000);
     }
+}
+
+void init_button(void)
+{
+    // init button
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    gpio_set_irq_enabled_with_callback(
+        BUTTON_PIN,
+        GPIO_IRQ_EDGE_FALL, 
+        1,
+        on_button_pressed);
+}
+
+void on_button_pressed(uint gpio, uint32_t events)
+{
+    // avoid double click caused interrupts
+    uint32_t flags = save_and_disable_interrupts();
+
+    // wait required duration to check if button is pressed
+    for (size_t i = 0; i < SAVE_POINT_BTN_DURATION_SEC * 2; i++)
+    {
+        busy_wait_ms(500);
+        if (gpio_get(gpio)) {
+            // do nothing as button was released
+            restore_interrupts(flags);
+            return;
+        }
+    }
+
+    // TODO: GET current coordinates from static field
+    // use mutex!!!
+
+    restore_interrupts(flags);
 }
